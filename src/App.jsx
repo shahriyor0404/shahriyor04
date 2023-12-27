6d89ee8987acc22a116731adc20ef9ee9f57c781
madumarov <javlon.88809@mail.com>
2023-12-27 11:25:34 +0500

    shahriyor cmd ignored

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   const [count, setCount] = useState(10);
 
   const increment = () => {
-    setCount((prev) => prev * 3);
+    setCount((prev) => prev + 3);
     setCount((prev) => prev + 1);
   };
 
@@ -16,12 +16,12 @@ const App = () => {
     setCount(0);
   };
   return (
-    <div className="w-[320px] mx-auto border border-black text-center p-3 mt-0">
+    <div className="w-[320px] flex flex-col mx-auto border bg border-black text-center p-3  ">
       {" "}
-      <h1 className="text-3xl font-bold mb-2 ">counter: {count}</h1>
+      <h1 className="text-3xl font-bold mb-5 ">counter: {count}</h1>
       <div className="space-x-2">
         <button
-          className="px-4 py-2 bg-green-700 text-white  rounded-lg"
+          className="px-4 py-2 bg-green-700 text-white  rounded-lg mb-5"
           onClick={decrement}
         >
           decrement
