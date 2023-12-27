2b2f7bcf819685a7646e360ee34699d37f6116a3
madumarov <javlon.88809@mail.com>
2023-12-27 11:23:54 +0500

    shahriyor

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(10);
+
+  const increment = () => {
+    setCount((prev) => prev * 3);
+    setCount((prev) => prev + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+  return (
+    <div className="w-[320px] mx-auto border border-black text-center p-3 mt-0">
+      {" "}
+      <h1 className="text-3xl font-bold mb-2 ">counter: {count}</h1>
+      <div className="space-x-2">
+        <button
+          className="px-4 py-2 bg-green-700 text-white  rounded-lg"
+          onClick={decrement}
+        >
+          decrement
+        </button>
+        <button
+          className="px-4 py-2 bg-yellow-500 text-white  rounded-lg"
+          onClick={reset}
+        >
+          reset
+        </button>
+        <button
+          className="px-4 py-2 bg-red-600 text-white  rounded-lg"
+          onClick={increment}
+        >
+          increment
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
